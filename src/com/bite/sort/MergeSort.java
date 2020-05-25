7c4b460cb4ae40b76dc37a88f0373de0a08062e8
tian12138yu <374940928@qq.com>
2020-05-25 21:05:02 +0800

    bite

--- src/com/bite/sort/MergeSort.java
+++ src/com/bite/sort/MergeSort.java
@@ -89,9 +89,67 @@ public class MergeSort {
         merge1(array,low,mid,high);
     }
 
+    /**
+     * 非递归归并排序
+     * @param array
+     */
+    public static void mergeSort(int[] array) {
+        for (int gap = 1; gap < array.length; gap *= 2) {
+            mergeNor(array,gap);
+        }
+    }
+
+    public static void mergeNor(int[] array,int gap) {
+        int[] ret = new int[array.length];
+        int k = 0;//ret的下标
+        int s1 = 0;
+        int e1 = s1 + gap - 1;
+        int s2 = e1 + 1;
+        int e2 = s2 + gap-1 < array.length ? s2+gap-1 :  array.length-1;
+
+        //1、肯定是有两个归并段的
+        while (s2 < array.length) {
+            //2、对应的s1位置和s2位置进行比较
+            while (s1 <= e1 && s2 <= e2) {
+                if(array[s1] <= array[s2]) {
+                    ret[k++] = array[s1++];
+                }else {
+                    ret[k++] = array[s2++];
+                }
+            }
+            //3、上述第2步在比较的过程当中，肯定会有一个下标先走完一个归并段
+
+            //4、判断是谁没走完，把剩下的数据拷贝到结果数组当中
+            while (s1 <= e1) {
+                ret[k++] = array[s1++];
+            }
+
+            while (s2 <= e2) {
+                ret[k++] = array[s2++];
+            }
+
+            //5、接着确定新的s1,e1,s2,e2
+            s1 = e2 + 1;
+            e1 = s1 + gap - 1;
+            s2 = e1 + 1;
+            e2 = s2+gap-1 < array.length ? s2+gap-1 :  array.length-1;
+        }
+
+        while (s1 < array.length){
+            ret[k++] = array[s1++];
+        }
+
+        for (int j = 0; j < ret.length; j++) {
+            array[j] = ret[j];
+        }
+
+
+
+    }
+
     public static void main(String[] args) {
         int[] elem = {5,6,1,3,8,9};
-        spilit(elem,0,elem.length-1);
+        mergeSort(elem);
         System.out.println(Arrays.toString(elem));
     }
 }
