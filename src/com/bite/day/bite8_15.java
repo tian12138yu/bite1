60ab0c58f13d8770d492f1b4994658162e663ac6
tian12138yu <374940928@qq.com>
2020-08-15 17:54:32 +0800

    bite

--- src/com/bite/day/bite8_15.java
+++ src/com/bite/day/bite8_15.java
@@ -1,7 +1,12 @@
 package com.bite.day;
 
 import com.bite.TreeNode;
+import com.sun.xml.internal.ws.api.ha.StickyFeature;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.*;
 
 /**
@@ -136,9 +141,150 @@ public class bite8_15 {
         return res;
     }
 
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
         char[] chars = {'a','a'};
         char[] chars1 = {'.','*'};
         System.out.println(match(chars, chars1));
     }
+
+    /**
+     * 1
+     * 2178 8712
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = "";
+        while ((s= br.readLine()) != null){
+            TreeMap<Long, Long> map = new TreeMap<>();
+
+//            BigInteger n =BigInteger.valueOf(Long.parseLong(s));
+            int n = Integer.parseInt(s);
+            if (n >= 8712){
+                map.put(2178L,8712L);
+            }
+            if (n >= 21978){
+                map.put(21978L,87912L);
+            }
+
+            if (n >= 219978){
+                map.put(219978L,879912L);
+            }
+
+            if (n >= 2199978){
+                map.put(2199978L,8799912L);
+            }
+
+            if (n >= 21782178){
+                map.put(21782178L,87128712L);
+            }
+            if (n >= 21999978){
+                map.put(21999978L,87999912L);
+            }
+            if (n >= 219999978){
+                map.put(219999978L,879999912L);
+            }
+//            for (long i = 100000001; i <= n; i++) {
+//                if (resver(i) == i * 4){
+//                    map.put(i,i*4);
+//                }
+//            }
+
+            System.out.println(map.size());
+            for (Map.Entry<Long,Long> entry : map.entrySet()){
+                System.out.println(entry.getKey() + " " + entry.getValue());
+            }
+
+        }
+    }
+
+    private static long resver(long i) {
+
+        StringBuilder sb = new StringBuilder(String.valueOf(i));
+        StringBuilder reverse = sb.reverse();
+
+        return Long.parseLong(reverse.toString());
+    }
+
+
+    public static void main3(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = "";
+        while ((s= br.readLine()) != null){
+            int n = Integer.parseInt(s);
+            HashMap<String,String> map = new HashMap<String,String>();
+            for (int i = 0; i < n; i++) {
+                String[] s1 = br.readLine().split(" ");
+                map.put(s1[0],s1[1]);
+            }
+            int count = 0;
+
+            for (Map.Entry<String,String> entry : map.entrySet()) {
+                HashMap<String, String> map1 = new HashMap<>(map);
+                String value = entry.getValue();
+                String key = entry.getKey();
+                map1.remove(key);
+                boolean b = dfs(value,key,map1);
+                if (b)count++;
+            }
+
+            System.out.println(count);
+
+        }
+    }
+
+    private static boolean dfs(String next, String root, HashMap<String, String> map) {
+
+        if (!map.containsKey(next))return false;
+        if (map.get(next).equals(root))return true;
+        String s = map.get(next);
+        map.remove(s);
+        return dfs(s,root,map);
+    }
+
+    static class Titck{
+        String qi;
+        String mu;
+
+        public Titck(String qi, String mu) {
+            this.qi = qi;
+            this.mu = mu;
+        }
+    }
+
+
+
+    public static void main4(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = "";
+        while ((s= br.readLine()) != null){
+            String[] s1 = s.split(" ");
+            int n = Integer.parseInt(s1[0]);//订单数量
+            int m = Integer.parseInt(s1[1]);//关系数量
+
+            Map<Integer,Integer> map = new HashMap<>();
+            List<Integer> one = new ArrayList<>();
+            List<Integer> two = new ArrayList<>();
+            List<Node> nodes = new ArrayList<>();
+            for (int i = 0; i < m; i++) {
+                String[] s2 = br.readLine().split(" ");
+                int x = Integer.parseInt(s1[0]);
+                int y = Integer.parseInt(s2[1]);
+                map.put(x,y);
+                one.add(x);
+                two.add(y);
+            }
+
+        }
+    }
+
+    class Node{
+        int x;
+        int y;
+        Node next;
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
 }
