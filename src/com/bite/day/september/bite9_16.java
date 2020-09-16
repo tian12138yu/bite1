b18e8ce63a268abe6e952a27c0c9f5e7b20fc71f
tian12138yu <374940928@qq.com>
2020-09-16 19:42:21 +0800

    bite

---
+++ src/com/bite/day/september/bite9_16.java
@@ -0,0 +1,53 @@
+package com.bite.day.september;
+
+import com.bite.TreeNode;
+
+/**
+ * @Author tjy
+ * @Date 2020/9/16 19:10
+ */
+
+public class bite9_16 {
+
+    /**
+     * 翻转一棵二叉树。
+     *
+     * 示例：
+     *
+     * 输入：
+     *
+     *      4
+     *    /   \
+     *   2     7
+     *  / \   / \
+     * 1   3 6   9
+     * 输出：
+     *
+     *      4
+     *    /   \
+     *   7     2
+     *  / \   / \
+     * 9   6 3   1
+     */
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        swap(root);
+        return root;
+    }
+
+    private void swap(TreeNode root) {
+        if (root == null)
+            return;
+
+        TreeNode cur = root.left;
+        root.left = root.right;
+        root.right = cur;
+
+        swap(root.left);
+        swap(root.right);
+    }
+
+
+}
