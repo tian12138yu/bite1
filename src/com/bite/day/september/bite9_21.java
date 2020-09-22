18d5705f8f8b02693670fae449181643fb47faf6
tian12138yu <374940928@qq.com>
2020-09-22 20:01:33 +0800

    bite

--- src/com/bite/day/september/bite9_21.java
+++ src/com/bite/day/september/bite9_21.java
@@ -1,6 +1,8 @@
 package com.bite.day.september;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * @Author tjy
@@ -16,5 +18,46 @@ public class bite9_21 {
         new Thread(() -> {
 
         });
+    }
+
+    public int[] pushIntArray (int[] arr, int pushOffset) {
+        // write code here
+        int length = arr.length;
+        if (pushOffset > length) {
+            pushOffset = pushOffset % length;
+        }
+        int[] res = new int[length];
+        for (int i = 0; i < length; i++) {
+            res[(i + pushOffset) % length] = arr[i];
+        }
+
+        return res;
+    }
+
+
+    public int sort (String inData) {
+        // write code here
+        String[] s = inData.split(" ");
+        int len = s.length;
+        int[] a= new int[len];
+        int[] a1 = new int[len];
+        for (int i = 0; i < len; i++) {
+            int x = Integer.parseInt(s[i]);
+            a[i] = x;
+            a1[i] = x;
+        }
+
+        Arrays.sort(a1);
+        int count = 0;
+        int y = 0;
+        for (int i = 0; i < len; i++) {
+            if (a[i] == a1[y]){
+                ++count;
+                ++y;
+            }
+        }
+
+        return len - count;
+
     }
 }
